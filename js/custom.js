71995bf6ed727ada9a49eab501019cfb023bcb4c
Ted Tomlinson <zumbino@gmail.com>
2014-01-08 02:04:58 -0800

    parse and JS

--- js/custom.js
+++ js/custom.js
@@ -23,7 +23,72 @@ jQuery(document).ready(function(){
 		'transitionOut'	: 'none',
 		'overlayOpacity'	:   0.8 //Opacity of the overlay (from 0 to 1; default - 0.3)
 	});
-	
+
+
+	// --------------------------------------------------------
+	//	Parse Code
+	// -------------------------------------------------------- 
+	var contactsCompleteHtml = '<h2> Thanks for contacting us. We\'ll be in touch shortly </h2>';
+	Parse.initialize("uXfsYkqe7KVGWABXnJ9Ulm09UrzI21KmhgC7RFMC", "5BMsy1FFGRD9X6nvPk4VU9arjrk6f3U6CSocE3mY");
+	var ContactObject = Parse.Object.extend("ContactObject");
+	var SOURCE_TYPE = {
+		TRIAL: 'TRIAL',
+		SUBSCRIBE: 'SUBSCRIBE',
+		BUY: 'BUY'
+	};
+
+	function freeTrial() {
+		var nameVal = $('.js-trialname').val();
+		var emailVal = $('.js-trialemail').val();
+		_postContact({
+			source: SOURCE_TYPE.TRIAL,
+			name: nameVal,
+			email: emailVal
+		});
+		$('.form-area').parent().html(contactsCompleteHtml);
+		return false;
+	}
+
+	function buyNow(e) {
+		$('.js-buybutton')
+			.click(function(e) {
+				$('.js-buybutton').parent().fadeOut(1000, function() {
+					$('.buttons-area').html(contactsCompleteHtml);
+				});
+				var emailVal = $('.js-buyemail').val();
+				_postContact({
+					source: SOURCE_TYPE.BUY,
+					email: emailVal
+				});
+				return false;
+			})
+			.text('Submit');
+
+		$('.js-buyemail')
+			.removeAttr('style')
+			.addClass('slideInRight');
+
+    return false;
+	}
+
+	function subscribe() {
+		var emailVal = $('.js-subscribeemail').val();
+		_postContact({
+			source: SOURCE_TYPE.SUBSCRIBE,
+			email: emailVal
+		});
+		$('#buttom-subscribe').parent().html(contactsCompleteHtml);
+		return false;
+	}
+
+  function _postContact(contactObj) {
+  	var newContact = new ContactObject();
+		newContact.save(contactObj);
+  }
+
+  $('.js-buybutton').click(buyNow);
+	$('#form-area').submit(freeTrial);
+	$('#buttom-subscribe').submit(subscribe);
 });
 
 
